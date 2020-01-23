3557f62f3a0d5d9c3c89b000099736f27b364dc8
Brian <brianvu7@gmail.com>
2020-01-23 14:37:03 -0600

    Finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,44 +4,52 @@ var inputField = document.querySelector("#input");
 var letterGuess = document.querySelector("#letterGuess");
 var generatedWord = document.querySelector("#word");
 
-var lettersGuessed = "";
-
 generateButton.addEventListener("click", function(){
     wordAnswer = words[Math.floor(Math.random() * words.length)];
     wordBlank = "";
+    lettersGuessed= "";
     counter = 0;
+    guessesRemaining = 10;
+    document.querySelector("#winDisplay").textContent = "Letters guessed incorrectly:";
+    document.querySelector("#guesses").textContent = "Guesses remaining: " + guessesRemaining;
+
     for(i = 0; i < wordAnswer.length; i++){
         wordBlank += "_ ";
     }
+
     generatedWord.textContent = wordBlank;
     wordArray = wordAnswer.split("");
     wordBlankArray = wordBlank.split("");
-    console.log(wordAnswer)
-    console.log(wordArray)
-    console.log(wordBlankArray)
 });
 
 inputForm.addEventListener("submit", function(event){
     event.preventDefault();
     userGuess = inputField.value.toUpperCase();
 
-    for(i = 0; i < wordAnswer.length; i++){
-        if(userGuess === wordArray[i]){
-            wordBlankArray[(i * 2)] = userGuess;
-            wordBlank = "";
-            for(j = 0; j < wordBlankArray.length; j++){
-                wordBlank += wordBlankArray[j];
+    if(userGuess.length === 1 && guessesRemaining > 0 && counter !== wordAnswer.length){
+        for(i = 0; i < wordAnswer.length; i++){
+            if(userGuess === wordArray[i]){
+                wordBlankArray[(i * 2)] = userGuess;
+                wordBlank = "";
+                for(j = 0; j < wordBlankArray.length; j++){
+                    wordBlank += wordBlankArray[j];
+                }
+                generatedWord.textContent = wordBlank;
+                counter++;
             }
-            generatedWord.textContent = wordBlank;
-            counter++;
-            console.log(wordBlank, wordBlank.length)
-            console.log(counter)
+            else if(lettersGuessed.indexOf(userGuess) === -1 && wordAnswer.indexOf(userGuess) === -1){
+                lettersGuessed += userGuess + " ";
+                letterGuess.textContent = lettersGuessed;
+                guessesRemaining--;
+                document.querySelector("#guesses").textContent = "Guesses remaining: " + guessesRemaining;
+            }
         }
-        else if(lettersGuessed.indexOf(userGuess) === -1){
-            lettersGuessed += userGuess;
-            letterGuess.textContent = lettersGuessed;
-            console.log("here")
-        }
+    }
+    else if(counter === wordAnswer.length || guessesRemaining === 0){
+        alert("Press 'Generate Word' to play again.");
+    }
+    else{
+        alert("Please guess 1 letter only.");
     }
 
     checkForWin();
@@ -50,7 +58,18 @@ inputForm.addEventListener("submit", function(event){
 function checkForWin(){
     if(counter === wordAnswer.length){
         letterGuess.textContent = "";
-        document.querySelector("#winDisplay").textContent = "YOU WON!";
+        var playAgain = document.createElement("p");
+        playAgain.textContent = "Press 'Generate Word' to play again.";
+        document.querySelector("#winDisplay").textContent = "YOU WIN!";
+        document.querySelector("#winDisplay").appendChild(playAgain);
+        return;
+    }
+    else if(guessesRemaining === 0){
+        letterGuess.textContent = "";
+        var playAgain = document.createElement("p");
+        playAgain.textContent = "Press 'Generate Word' to play again.";
+        document.querySelector("#winDisplay").textContent = "YOU LOSE!";
+        document.querySelector("#winDisplay").appendChild(playAgain);
         return;
     }
 }
