10b7649fec15a5d5815c007123da3efe795da91a
Brian <brianvu7@gmail.com>
2020-01-23 03:04:25 -0600

    Fixed guess display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,56 @@
 var words = ["AMERICA", "BULGARIA", "SPAIN", "MEXICO", "CANADA", "VIETNAM", "AUSTRALIA", "VENEZUELA", "TAIWAN", "GERMANY", "THAILAND", "MADAGASCAR", "SINGAPORE"]
+var generateButton = document.querySelector("#generateButton");
+var inputField = document.querySelector("#input");
+var letterGuess = document.querySelector("#letterGuess");
+var generatedWord = document.querySelector("#word");
 
-function generateWord(){
-    var wordBlank = "";
-    var getWord = words[Math.floor(Math.random() * words.length)]
+var lettersGuessed = "";
 
-    for(i = 0; i < getWord.length; i++){
-        wordBlank += "_"
-        document.getElementById("word").innerHTML = wordBlank;
+generateButton.addEventListener("click", function(){
+    wordAnswer = words[Math.floor(Math.random() * words.length)];
+    wordBlank = "";
+    counter = 0;
+    for(i = 0; i < wordAnswer.length; i++){
+        wordBlank += "_ ";
+    }
+    generatedWord.textContent = wordBlank;
+    wordArray = wordAnswer.split("");
+    wordBlankArray = wordBlank.split("");
+    console.log(wordAnswer)
+    console.log(wordArray)
+    console.log(wordBlankArray)
+});
+
+inputForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    userGuess = inputField.value.toUpperCase();
+
+    for(i = 0; i < wordAnswer.length; i++){
+        if(userGuess === wordArray[i]){
+            wordBlankArray[(i * 2)] = userGuess;
+            wordBlank = "";
+            for(j = 0; j < wordBlankArray.length; j++){
+                wordBlank += wordBlankArray[j];
+            }
+            generatedWord.textContent = wordBlank;
+            counter++;
+            console.log(wordBlank, wordBlank.length)
+            console.log(counter)
+        }
+        else if(lettersGuessed.indexOf(userGuess) === -1){
+            lettersGuessed += userGuess;
+            letterGuess.textContent = lettersGuessed;
+            console.log("here")
+        }
     }
 
-    console.log(getWord);
+    checkForWin();
+});
 
-    document.onkeypress = function(event){
-        var keyPress = String.fromCharCode(event.keyCode);
-        var keyPressU = keyPress.toUpperCase();
-        document.getElementById("letterGuess").innerHTML = keyPressU;
-        
-        for(i = 0; i < wordBlank.length; i++){
-            
-            if(keyPressU === getWord.charAt(i)){
-
-                var replacedWord = wordBlank.replace(wordBlank.charAt(i), keyPressU);
-                document.getElementById("word").innerHTML = replacedWord;
-            }
-        }
+function checkForWin(){
+    if(counter === wordAnswer.length){
+        letterGuess.textContent = "";
+        document.querySelector("#winDisplay").textContent = "YOU WON!";
+        return;
     }
 }
